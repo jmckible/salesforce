b562eb5dbd8ccb9f0e7b055474d3736467923ea9
Jordan McKible <jordan@mckible.com>
2008-07-24 13:49:22 -0700

    Add company to leads.

--- lib/salesforce/lead.rb
+++ lib/salesforce/lead.rb
@@ -1,11 +1,11 @@
 module Salesforce
   class Lead < Base
     
-    attr_accessor :first_name, :last_name, :email
+    attr_accessor :first_name, :last_name, :email, :company
     
     class << self
       def columns
-        {:Id=>:id, :FirstName=>:first_name, :LastName=>:last_name, :Email=>:email}
+        {:Id=>:id, :FirstName=>:first_name, :LastName=>:last_name, :Email=>:email, :Company=>:company}
       end
     end
     
--- spec/live/harvester.rb
+++ spec/live/harvester.rb
@@ -6,7 +6,6 @@ describe 'harvester' do
   it 'should harvest' do
     person = Person.new :salesforce_email=>ENV['SF_EMAIL'], :salesforce_password=>ENV['SF_PASSWORD'], :salesforce_api_key=>ENV['SF_API_KEY']
     person.establish_salesforce_session.should be_true    
-    #leads = person.salesforce_session.leads.find :all
-    person.salesforce_session.queryMore :queryLocator=>'01g70000002cRQJAA2'
+    leads = person.salesforce_session.leads.find :all
   end
 end
