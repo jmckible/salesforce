69a5404005b55791d6483779c6b512d261e3b36f
Jordan McKible <jordan@mckible.com>
2008-07-23 21:20:46 -0700

    Spec for parsing an empty soap response.

--- spec/soap_response_spec.rb
+++ spec/soap_response_spec.rb
@@ -3,7 +3,13 @@ require File.dirname(__FILE__) + '/helper'
 describe Salesforce::SoapResponse, 'parsing' do
   it 'should handle an error response'
   
-  it 'should build a response from an empty result'
+  it 'should build a response from an empty result' do
+    xml = IO.read(File.dirname(__FILE__) + '/fixtures/empty.xml')
+    soap_response = Salesforce::SoapResponse.new xml
+    soap_response.queryResponse.result.done.should == 'true'
+    soap_response.queryResponse.result[:queryLocator].should be_nil
+    soap_response.queryResponse.result[:size].should == '0'
+  end
   
   it 'should build a response from single result' do
     xml = IO.read(File.dirname(__FILE__) + '/fixtures/account.xml')
